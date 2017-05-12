8a6e2580759421225a2a734b1c2ecd1f18dd2eca
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-05-11 22:06:06 -0700

    AJAX for follow and like

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,24 +1,33 @@
 class FollowingsController < ApplicationController
   before_action :authenticate_user!
   def create
-    followed = User.find(params[:user_id])
-    following = Following.new(user_id: followed.id, follower_id: current_user.id)
+    @followed = User.find(params[:user_id])
+    @following = Following.new(user_id: @followed.id, follower_id: current_user.id)
 
-    if following.save
-      redirect_to user_path(followed), notice: "You just followed #{followed.first_name.capitalize}!"
-    else
-      redirect_to root_path, alert: following.errors.full_messages.join(', ')
+    respond_to do |format|
+      if @following.save
+        format.html {redirect_to user_path(@followed), notice: "You just followed #{@followed.first_name.capitalize}!"}
+        format.js {render :render_follow}
+      else
+        format.html {redirect_to root_path, alert: @following.errors.full_messages.join(', ')}
+        format.js {render :render_follow}
+      end
     end
   end
 
   def destroy
-    byebug
-    following = Following.find(params[:id])
-    user = following.follower_id
-    if following.destroy
-      redirect_to user_followers_path(user), notice:'unfollowed'
-    else
-      redirect_to user_followers_path(user), alert:following.errors.full_messages.join(', ')
+    @following = Following.find(params[:id])
+    @followed = User.find(@following.user_id)
+    user = @following.follower_id
+
+    respond_to do |format|
+      if @following.destroy
+        format.html {redirect_to user_followers_path(user), notice:'unfollowed'}
+        format.js {render :render_follow}
+      else
+        format.html {redirect_to user_followers_path(user), alert:@following.errors.full_messages.join(', ')}
+        format.js {render :render_follow}
+      end
     end
   end
   # def destroy
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,36 +1,38 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   def create
-    post = Post.find(params[:post_id])
-    if cannot? :like, post
-      redirect_to post_path(post), alert: 'liking your own post is disgusting'
+    @post = Post.find(params[:post_id])
+    if cannot? :like, @post
+      redirect_to post_path(@post), alert: 'liking your own post is disgusting'
       return
     end
 
-    like = Like.new(user: current_user, post: post)
+    @like = Like.new(user: current_user, post: @post)
+
     respond_to do |format|
-      if like.save
-        format.html {redirect_to post_path(post), notice: 'post liked'}
+      if @like.save
+        format.html {redirect_to post_path(@post), notice: 'post liked'}
         format.js {render :render_like}
       else
-        format.html {redirect_to post_path(post), alert: like.errors.full_messages.join(', ')}
+        format.html {redirect_to post_path(@post), alert: @like.errors.full_messages.join(', ')}
         format.js {render :render_like}
       end
     end
   end
 
   def destroy
-    like = Like.find(params[:id])
-    if cannot? :like, like.post
-      redirect_to post_path(like.post), alert: 'Can not Unliking your own post'
+    @post = Post.find(params[:post_id])
+    @like = Like.find(params[:id])
+    if cannot? :like, @like.post
+      redirect_to post_path(@like.post), alert: 'Can not Unliking your own post'
       return
     end
     respond_to do |format|
-      if like.destroy
-        format.html {redirect_to post_path(like.post), notice:'Un-liked post!'}
+      if @like.destroy
+        format.html {redirect_to post_path(@like.post), notice:'Un-liked post!'}
         format.js {render :render_like}
       else
-        format.html {redirect_to post_path(like.post), alert: like.errors.full_messages.join(', ')}
+        format.html {redirect_to post_path(@like.post), alert: @like.errors.full_messages.join(', ')}
         format.js {render :render_like}
       end
     end
